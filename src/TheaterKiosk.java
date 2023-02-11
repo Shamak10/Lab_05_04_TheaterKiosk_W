41445ceb66c1b1acc4eef9d819bb22b8745e0537
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-02-11 11:25:10 -0500

    Update TheaterKiosk.java

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,18 +1,15 @@
-import java.util.Scanner;
 public class TheaterKiosk {
     public static void main(String[] args) {
-        Scanner AgeInput = new Scanner(System.in);
-        int UserAge;
+        int userAge = 10; //It is asking for your age
 
         System.out.println("Welcome to the theater");// welcomes you to the theater
-        System.out.print("Please Enter your age: 20 ");//It is asking for your age
-        UserAge = 22;// your age
+        System.out.println("Your age is: " + userAge);//outputs your age
 
-        if (UserAge >= 22) {
+        if (userAge >= 21) {
             System.out.println("You got a wrist band!");//output get a wristband above 21 years or older
-        if (UserAge<21)    {
-            System.out.println("you are underage and do not get Wristband");//No wristband for under 21 years or less
-        }
+        } else if (userAge < 21) {
+            System.out.println("You are underage and do not get a wristband.");
+            //No wristband for under 21 years or less
         }
     }
 }
